7d2ccea762e5a75bd0d43062aa7fafb1609e30b1
Hailey0930 <hyerin0930@gmail.com>
2022-09-06 16:27:20 +0900

    :lipstick: New: user myPage css #2

---
+++ src/components/units/my-page/my-page.presenter.tsx
@@ -0,0 +1,83 @@
+import * as S from "./my-page.styles";
+import { ImHeart } from "react-icons/im";
+
+export default function MyPagePresenter() {
+  return (
+    <S.Wrapper>
+      <S.ProfileWrapper>
+        <S.CategoryTitle>아이디</S.CategoryTitle>
+        <S.ProfileContents>aaa@aaa.com</S.ProfileContents>
+        <S.CategoryTitle>닉네임</S.CategoryTitle>
+        <S.ProfileContents>철수</S.ProfileContents>
+        <S.CategoryTitle>보유 포인트</S.CategoryTitle>
+        <S.ProfileContents>0 P</S.ProfileContents>
+      </S.ProfileWrapper>
+
+      <S.CategoryWrapper>
+        <S.CategoryTitle>내 전문가</S.CategoryTitle>
+        <S.ExpertWrapper>
+          <S.ExpertBody>
+            <S.ExpertPhoto></S.ExpertPhoto>
+            <S.ExpertDetail>
+              <S.CategoryContents>이름 : </S.CategoryContents>
+              <S.CategoryContents>소개 : </S.CategoryContents>
+              <S.CategoryContents>상담 비용 : </S.CategoryContents>
+            </S.ExpertDetail>
+          </S.ExpertBody>
+
+          <S.ExpertButtonWrapper>
+            <S.ExpertButton>상담하기</S.ExpertButton>
+            <S.ExpertButton>리뷰쓰기</S.ExpertButton>
+          </S.ExpertButtonWrapper>
+        </S.ExpertWrapper>
+
+        <S.ExpertWrapper>
+          <S.ExpertBody>
+            <S.ExpertPhoto></S.ExpertPhoto>
+            <S.ExpertDetail>
+              <S.CategoryContents>이름 : </S.CategoryContents>
+              <S.CategoryContents>소개 : </S.CategoryContents>
+              <S.CategoryContents>상담 비용 : </S.CategoryContents>
+            </S.ExpertDetail>
+          </S.ExpertBody>
+
+          <S.ExpertButtonWrapper>
+            <S.ExpertButton>상담하기</S.ExpertButton>
+            <S.ExpertButton>리뷰쓰기</S.ExpertButton>
+          </S.ExpertButtonWrapper>
+        </S.ExpertWrapper>
+      </S.CategoryWrapper>
+
+      <S.CategoryWrapper>
+        <S.CategoryTitle>활동 기록</S.CategoryTitle>
+        <S.HistoryCategoryWrapper>
+          <S.HistoryButton>사연 목록</S.HistoryButton>
+          <S.HistoryButton>댓글 목록</S.HistoryButton>
+          <S.HistoryButton>공감 목록</S.HistoryButton>
+        </S.HistoryCategoryWrapper>
+        <S.HistoryBody>
+          <S.CategoryContents>
+            왜 자꾸 기분이 가라앉는지 모르겠어요...
+          </S.CategoryContents>
+          <S.HistoryInfoWrapper>
+            <ImHeart
+              style={{ width: "13px", height: "13px", color: "#F293A3" }}
+            />
+            <S.HistoryInfo>1 명이 공감</S.HistoryInfo>
+          </S.HistoryInfoWrapper>
+        </S.HistoryBody>
+        <S.HistoryBody>
+          <S.CategoryContents>
+            왜 자꾸 기분이 가라앉는지 모르겠어요...
+          </S.CategoryContents>
+          <S.HistoryInfoWrapper>
+            <ImHeart
+              style={{ width: "13px", height: "13px", color: "#F293A3" }}
+            />
+            <S.HistoryInfo>1 명이 공감</S.HistoryInfo>
+          </S.HistoryInfoWrapper>
+        </S.HistoryBody>
+      </S.CategoryWrapper>
+    </S.Wrapper>
+  );
+}

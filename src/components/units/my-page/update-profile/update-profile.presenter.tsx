fc959beac472c32c51a7722c732d49414854ca3a
Hailey0930 <hyerin0930@gmail.com>
2022-09-12 16:51:09 +0900

    :lipstick: Edit: 유저 마이페이지 CSS 수정 #2

--- src/components/units/my-page/update-profile/update-profile.presenter.tsx
+++ src/components/units/my-page/update-profile/update-profile.presenter.tsx
@@ -1,4 +1,8 @@
+import BasicButton from "../../../commons/button";
 import * as S from "./update-profile.styles";
+import { HiMail } from "react-icons/hi";
+import { BsFillPersonFill } from "react-icons/bs";
+import { RiLockPasswordFill } from "react-icons/ri";
 import { IUpdateProfilePresenterProps } from "./update-profile.types";
 
 export default function UpdateProfilePresenter(
@@ -7,27 +11,86 @@ export default function UpdateProfilePresenter(
   return (
     <form onSubmit={props.handleSubmit(props.onClickProfileEditButton)}>
       <S.Wrapper>
-        <S.CategoryTitle>아이디</S.CategoryTitle>
-        <S.EditInput
-          {...props.register("email")}
-          defaultValue={props.UserInfo?.fetchLoginUser.email}
-        ></S.EditInput>
+        <S.ProfileBody>
+          <S.CategoryTitle>
+            <HiMail
+              style={{
+                width: "25px",
+                height: "23px",
+                marginRight: "5px",
+                color: "#808080",
+              }}
+            />
+            아이디
+          </S.CategoryTitle>
+          <S.EditInput
+            {...props.register("email")}
+            defaultValue={props.UserInfo?.fetchLoginUser.email}
+          ></S.EditInput>
+        </S.ProfileBody>
         <S.ErrorMessage>{props.formState.errors.email?.message}</S.ErrorMessage>
-        <S.CategoryTitle>닉네임</S.CategoryTitle>
-        <S.EditInput
-          {...props.register("nickname")}
-          defaultValue={props.UserInfo?.fetchLoginUser.nickname}
-        ></S.EditInput>
+
+        <S.ProfileBody>
+          <S.CategoryTitle>
+            <BsFillPersonFill
+              style={{
+                width: "25px",
+                height: "23px",
+                marginRight: "5px",
+                color: "#808080",
+              }}
+            />
+            닉네임
+          </S.CategoryTitle>
+          <S.EditInput
+            {...props.register("nickname")}
+            defaultValue={props.UserInfo?.fetchLoginUser.nickname}
+          ></S.EditInput>
+        </S.ProfileBody>
         <S.ErrorMessage>
           {props.formState.errors.nickname?.message}
         </S.ErrorMessage>
-        <S.ButtonWrapper>
-          <S.ExpertButton type="button" onClick={props.onClickBackButton}>
-            돌아가기
-          </S.ExpertButton>
-          <S.ExpertButton type="submit">수정하기</S.ExpertButton>
-        </S.ButtonWrapper>
+
+        <S.ProfileBody>
+          <S.CategoryTitle>
+            <RiLockPasswordFill
+              style={{
+                width: "25px",
+                height: "23px",
+                marginRight: "5px",
+                color: "#808080",
+              }}
+            />
+            비밀번호
+          </S.CategoryTitle>
+          <S.EditInput></S.EditInput>
+        </S.ProfileBody>
+
+        <S.ProfileBody>
+          <S.CategoryTitle>
+            <RiLockPasswordFill
+              style={{
+                width: "25px",
+                height: "23px",
+                marginRight: "5px",
+                color: "#808080",
+              }}
+            />
+            비밀번호 확인
+          </S.CategoryTitle>
+          <S.EditInput></S.EditInput>
+        </S.ProfileBody>
       </S.Wrapper>
+
+      <S.ButtonWrapper>
+        <BasicButton
+          type="button"
+          onClick={props.onClickBackButton}
+          title="돌아가기"
+        ></BasicButton>
+        <BasicButton type="submit" title="프로필 수정"></BasicButton>
+        <BasicButton type="button" title="비밀번호 수정"></BasicButton>
+      </S.ButtonWrapper>
     </form>
   );
 }
--- src/components/units/my-page/update-profile/update-profile.styles.ts
+++ src/components/units/my-page/update-profile/update-profile.styles.ts
@@ -1,34 +1,46 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
+  width: 100%;
+  background-color: #f5f5f5;
+  border: 1px solid #c5c5c5;
+  padding: 10px 30px;
+  margin-bottom: 10px;
+`;
+
+export const ProfileBody = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 
   width: 100%;
+  padding: 5px 5px 0px 5px;
 `;
 
 export const CategoryTitle = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: flex-start;
+  align-items: center;
 
-  width: 100%;
-  font-size: 1rem;
-  padding: 5px 10px;
+  font-size: 1.0625rem;
+  font-weight: 700;
+  width: 50%;
+  padding: 5px 0px;
 `;
 
 export const EditInput = styled.input`
-  width: 80%;
-  height: 30px;
-  padding: 10px;
-  margin-left: 20px;
-  margin-bottom: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+
+  width: 50%;
 `;
 
 export const ErrorMessage = styled.div`
-  width: 80%;
-  margin-left: 20px;
+  width: 55%;
+  margin-left: 47%;
   color: red;
   font-size: 0.7rem;
 `;
@@ -36,26 +48,8 @@ export const ErrorMessage = styled.div`
 export const ButtonWrapper = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-between;
+
   width: 100%;
-`;
-
-export const ExpertButton = styled.button`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-
-  width: 100px;
-  height: 30px;
-  margin-right: 15px;
-  font-size: 0.85rem;
-  background-color: #9cd6ff;
-  border-radius: 20px;
-  border: none;
-  cursor: pointer;
-
-  :hover {
-    background-color: #6fe5ff;
-  }
+  padding: 5px 10px;
 `;

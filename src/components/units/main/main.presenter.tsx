01b2ccaa98d9c3823377a1fe7d86ad90ba0ee3f1
= <dnsghk2@gmail.com>
2022-09-12 19:27:01 +0900

    :lipstick: Add: 메인페이지 마크업 #53

--- src/components/units/main/main.presenter.tsx
+++ src/components/units/main/main.presenter.tsx
@@ -4,11 +4,14 @@ import "slick-carousel/slick/slick-theme.css";
 import * as S from "./main.styles";
 
 export default function MainUI() {
+  const list = [1, 2, 3, 4];
+  const title = ["전문가 상담", "커뮤니티", "마이페이지", "포스트잇"];
+
   const settings = {
-    dots: true,
-    infinite: true,
     speed: 500,
     arrows: false,
+    autoplay: true,
+    loop: true,
     slidesToShow: 1,
     slidesToScroll: 1,
   };
@@ -16,26 +19,111 @@ export default function MainUI() {
   return (
     <S.Wrapper>
       <Slider
-        dots={settings.dots}
-        infinite={settings.infinite}
         speed={settings.speed}
         arrows={settings.arrows}
+        autoplay={settings.autoplay}
         slidesToShow={settings.slidesToShow}
         slidesToScroll={settings.slidesToScroll}
       >
         <div>
-          <S.SliderItem>베스트 게시글</S.SliderItem>
+          <S.SliderItem1>
+            <img src="/main/메인1.png" />
+            <img src="/main/사람1.png" />
+          </S.SliderItem1>
         </div>
         <div>
-          <S.SliderItem>베스트 게시글</S.SliderItem>
-        </div>
-        <div>
-          <S.SliderItem>베스트 게시글</S.SliderItem>
+          <S.SliderItem2>
+            <img src="/main/메인2.png" />
+            <img src="/main/사람2.png" />
+          </S.SliderItem2>
         </div>
       </Slider>
+      <S.Category>
+        {list.map((el) => (
+          <div key={el}>
+            <img src={`/main/icon${el}.png`} />
+            <span>{`${title[el - 1]}`}</span>
+          </div>
+        ))}
+      </S.Category>
+      <S.Reviews>
+        <S.Title>베스트 사연</S.Title>
+        <Slider
+          speed={settings.speed}
+          arrows={settings.arrows}
+          slidesToShow={settings.slidesToShow}
+          slidesToScroll={settings.slidesToScroll}
+        >
+          <div>
+            <S.ReviewItem>
+              <S.ExpertName>오박사</S.ExpertName>
+              <S.ReviewCont>피카츄 주셔서 감사합니다~!!</S.ReviewCont>
+              <S.UserProfile>
+                <S.NickName>지우</S.NickName>
+                <S.CreatedAt>22년 9월 12일</S.CreatedAt>
+              </S.UserProfile>
+              <S.MoveBtn>해당 전문가 보러 가기</S.MoveBtn>
+            </S.ReviewItem>
+          </div>
+          <div>
+            <S.ReviewItem>
+              <S.ExpertName>오박사</S.ExpertName>
+              <S.ReviewCont>피카츄 주셔서 감사합니다~!!</S.ReviewCont>
+              <S.UserProfile>
+                <S.NickName>지우</S.NickName>
+                <S.CreatedAt>22년 9월 12일</S.CreatedAt>
+              </S.UserProfile>
+              <S.MoveBtn>해당 전문가 보러 가기</S.MoveBtn>
+            </S.ReviewItem>
+          </div>
+        </Slider>
+      </S.Reviews>
+      <S.Experts>
+        <S.ExpertsTitle>등록한 전문가들</S.ExpertsTitle>
+        <S.SemiTitle>
+          당신의 고민을 해결해줄 전문가들을 만나보세요!!
+        </S.SemiTitle>
+        <Slider
+          speed={settings.speed}
+          arrows={settings.arrows}
+          slidesToShow={settings.slidesToShow}
+          slidesToScroll={settings.slidesToScroll}
+        >
+          <div>
+            <S.ExpertsItem>
+              <S.Shape>
+                <S.ItemImage src="/main/오박사.jpeg" />
+              </S.Shape>
+              <h3>오박사</h3>
+              <S.Job>포켓몬 박사</S.Job>
+              <S.Carrier>
+                온갖 포켓몬을 30년간 연구해서 잘 알고 있습니다.
+              </S.Carrier>
+            </S.ExpertsItem>
+          </div>
+          <div>
+            <S.ExpertsItem>
+              <S.Shape>
+                <S.ItemImage src="/main/오박사.jpeg" />
+              </S.Shape>
+              <h3>오박사</h3>
+              <S.Job>포켓몬 박사</S.Job>
+              <S.Carrier>
+                온갖 포켓몬을 30년간 연구해서 잘 알고 있습니다.
+              </S.Carrier>
+            </S.ExpertsItem>
+          </div>
+        </Slider>
+      </S.Experts>
       <S.Buttons>
-        <button>공지사항</button>
-        <button>FAQ</button>
+        <div>
+          <S.OfficialBtn></S.OfficialBtn>
+          <span>공지사항</span>
+        </div>
+        <div>
+          <S.FaqBtn></S.FaqBtn>
+          <span>FAQ</span>
+        </div>
       </S.Buttons>
     </S.Wrapper>
   );
--- src/components/units/main/main.styles.ts
+++ src/components/units/main/main.styles.ts
@@ -3,36 +3,202 @@ import styled from "@emotion/styled";
 export const Wrapper = styled.div`
   width: 100vw;
   max-width: 414px;
+  overflow-x: hidden;
 
   & button {
     cursor: pointer;
     text-align: center;
     padding: 12px 14px;
+    border-radius: 10px;
+    border: none;
   }
 `;
 
-export const SliderItem = styled.div`
-  height: 240px;
+export const SliderItem1 = styled.div`
+  height: 160px;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
+  background-image: url("/main/배경1.png");
+`;
+
+export const SliderItem2 = styled.div`
+  height: 160px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-image: url("/main/배경2.png");
+`;
+
+export const Category = styled.div`
+  margin-top: 50px;
+  display: flex;
+  justify-content: space-around;
+
+  & div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  & img {
+    width: 64px;
+    height: 64px;
+    cursor: pointer;
+    margin-bottom: 10px;
+  }
+`;
+
+export const Reviews = styled.div`
+  width: 100%;
+  margin-top: 20px;
+  padding: 1.7rem;
+  font-weight: 700;
+  font-size: 1.2rem;
+  background-color: #f5f7fa;
+  width: 414px;
+  height: 350px;
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 20px;
+`;
+
+export const ReviewItem = styled.div`
+  width: 326px;
+  height: 227px;
+  background-color: #ffffff;
+  padding: 1.2rem;
+  border-radius: 1.7rem;
+`;
+
+export const ExpertName = styled.h2`
+  font-size: 1.2rem;
+  color: #2b2ab8;
+`;
+
+export const ReviewCont = styled.p`
+  font-size: 1rem;
+  color: #c7c8cb;
+  height: 100px;
+  padding: 30px 0;
+`;
+
+export const UserProfile = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 0.7rem;
+  color: #c7c8cb;
+`;
+
+export const NickName = styled.h2`
+  font-size: 0.9rem;
+  color: #acaeb2;
+`;
+
+export const CreatedAt = styled.span``;
+
+export const MoveBtn = styled.span`
+  background: none;
+  font-size: 1rem;
+  position: relative;
+  left: 150px;
+  bottom: -30px;
+  color: #99dce0;
+`;
+
+export const Experts = styled.div`
+  margin-top: 1.7rem;
+  padding: 1.2rem;
+`;
+
+export const ExpertsTitle = styled.h1`
+  font-size: 1.2rem;
+  font-weight: 700;
+`;
+
+export const SemiTitle = styled.h2`
+  color: #c7c8cb;
+  margin: 0.7rem 0 1.2rem;
+`;
+
+export const ExpertsItem = styled.div`
+  width: 250px;
+  height: 350px;
+  box-shadow: 0 10px 20px #bee9eb;
+  text-align: center;
+  overflow: hidden;
+  margin: 1rem auto;
+
+  & h3 {
+    margin-bottom: 10px;
+    font-size: 1.7rem;
+  }
+`;
+
+export const ItemImage = styled.img`
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  border: 8px solid white;
+  position: relative;
+  top: 145px;
+  left: 140px;
+  box-shadow: 0 2px 15px rgb(58, 54, 54);
+  transform: rotate(-20deg);
+`;
+
+export const Shape = styled.div`
+  width: 350px;
+  height: 200px;
+  background: linear-gradient(
+    9deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(153, 224, 208, 1) 0%,
+    rgba(227, 240, 228, 1) 71%
+  );
+  margin-left: -20px;
+  position: relative;
+  top: -80px;
+  transform: rotate(25deg);
+`;
+
+export const Job = styled.h2`
+  color: rgb(105, 100, 109);
+  margin-bottom: 0.7rem;
+`;
+
+export const Carrier = styled.p`
+  padding-left: 30px;
+  padding-right: 30px;
+  color: rgb(105, 100, 109);
+  line-height: 1.2rem;
 `;
 
 export const Buttons = styled.div`
-  margin-top: 100px;
-  margin-bottom: 50px;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
+  text-align: center;
+  margin: 1.5rem;
+  text-shadow: 1px solid black;
+  font-size: 1.2rem;
+  font-weight: 900;
+  color: #99dce0;
+`;
 
-  & button {
-    width: 150px;
+export const OfficialBtn = styled.button`
+  width: 11rem;
+  height: 8rem;
+  margin-bottom: 1.2rem;
 
-    cursor: pointer;
-    text-align: center;
-    background-color: #9cd6ff;
-    border: none;
-    :hover {
-      background-color: #6fe5ff;
-    }
-  }
+  background: url("/main/공지사항.jpg") center center no-repeat;
+  background-size: cover;
+`;
+
+export const FaqBtn = styled.button`
+  width: 11rem;
+  height: 8rem;
+  background: url("/main/FAQ.jpg") center center no-repeat;
+  background-size: cover;
+  margin-bottom: 1.2rem;
 `;

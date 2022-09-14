f788220ae827009c1a274b46da7ef1ef84cc13d2
Hailey0930 <hyerin0930@gmail.com>
2022-09-15 00:16:29 +0900

    :art: Add: 전문가 리스트 필터 적용, fetch, 디테일 이동, 무한스크롤 적용,  CSS 수정 #40

--- src/components/units/expert/list/ExpertList.presenter.tsx
+++ src/components/units/expert/list/ExpertList.presenter.tsx
@@ -1,5 +1,7 @@
 import * as S from "./ExpertList.styles";
 import { IExpertListPresenterProps } from "./ExpertList.types";
+import { AiFillStar } from "react-icons/ai";
+import InfiniteScroll from "react-infinite-scroller";
 
 export default function ExpertListUi(props: IExpertListPresenterProps) {
   return (
@@ -18,25 +20,141 @@ export default function ExpertListUi(props: IExpertListPresenterProps) {
           가격순
         </S.PriceFilterButton>
       </S.FetchWrapper>
-      <S.ListWrapper>
-        <S.Image />
-        <S.ProfileWrapper>
-          <S.NameWrapper>
-            <S.NameTitle>이름</S.NameTitle>
-            <S.Name>김박사 </S.Name>
-          </S.NameWrapper>
-          <S.IntroductionWrapper>
-            <S.IntroductionTitle>소개</S.IntroductionTitle>
-            <S.Introduction>
-              안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
-            </S.Introduction>
-          </S.IntroductionWrapper>
-          <S.PriceWrapper>
-            <S.PriceTitle>비용</S.PriceTitle>
-            <S.Price>50000원</S.Price>
-          </S.PriceWrapper>
-        </S.ProfileWrapper>
-      </S.ListWrapper>
+
+      {props.time && (
+        <S.ListWrapper>
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={props.FetchMoreTimeData}
+            hasMore={true}
+            useWindow={false}
+            style={{ width: "100%" }}
+          >
+            {props.timeData?.fetchSpecialists.map((el) => (
+              <S.MapWrapper key={el.id}>
+                <S.TopWrapper onClick={props.onClickMoveToDetail} id={el.id}>
+                  <S.Image />
+                  <S.ProfileWrapper>
+                    <S.NameWrapper>
+                      <S.NameTitle>이름</S.NameTitle>
+                      <S.Name>{el.name}</S.Name>
+                    </S.NameWrapper>
+                    <S.IntroductionWrapper>
+                      <S.IntroductionTitle>소개</S.IntroductionTitle>
+                      <S.Introduction>{el.summary}</S.Introduction>
+                    </S.IntroductionWrapper>
+                    <S.PriceWrapper>
+                      <S.PriceTitle>비용</S.PriceTitle>
+                      <S.Price>{el.price.toLocaleString()}원</S.Price>
+                    </S.PriceWrapper>
+                  </S.ProfileWrapper>
+                </S.TopWrapper>
+                <S.BottomWrapper>
+                  <AiFillStar
+                    style={{
+                      width: "27px",
+                      height: "23px",
+                      color: "#73bea8",
+                      marginRight: "5px",
+                    }}
+                  />{" "}
+                  평균 {el.averageRate} 점
+                </S.BottomWrapper>
+              </S.MapWrapper>
+            ))}
+          </InfiniteScroll>
+        </S.ListWrapper>
+      )}
+
+      {props.review && (
+        <S.ListWrapper>
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={props.FetchMoreReviewData}
+            hasMore={true}
+            useWindow={false}
+            style={{ width: "100%" }}
+          >
+            {props.reviewData?.fetchSpecialsitByRate.map((el) => (
+              <S.MapWrapper key={el.id}>
+                <S.TopWrapper onClick={props.onClickMoveToDetail} id={el.id}>
+                  <S.Image />
+                  <S.ProfileWrapper>
+                    <S.NameWrapper>
+                      <S.NameTitle>이름</S.NameTitle>
+                      <S.Name>{el.name}</S.Name>
+                    </S.NameWrapper>
+                    <S.IntroductionWrapper>
+                      <S.IntroductionTitle>소개</S.IntroductionTitle>
+                      <S.Introduction>{el.summary}</S.Introduction>
+                    </S.IntroductionWrapper>
+                    <S.PriceWrapper>
+                      <S.PriceTitle>비용</S.PriceTitle>
+                      <S.Price>{el.price.toLocaleString()}원</S.Price>
+                    </S.PriceWrapper>
+                  </S.ProfileWrapper>
+                </S.TopWrapper>
+                <S.BottomWrapper>
+                  <AiFillStar
+                    style={{
+                      width: "27px",
+                      height: "23px",
+                      color: "#73bea8",
+                      marginRight: "5px",
+                    }}
+                  />{" "}
+                  평균 {el.averageRate} 점
+                </S.BottomWrapper>
+              </S.MapWrapper>
+            ))}
+          </InfiniteScroll>
+        </S.ListWrapper>
+      )}
+
+      {props.price && (
+        <S.ListWrapper>
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={props.FetchMorePriceData}
+            hasMore={true}
+            useWindow={false}
+            style={{ width: "100%" }}
+          >
+            {props.priceData?.fetchSpecialistByPrice.map((el) => (
+              <S.MapWrapper key={el.id}>
+                <S.TopWrapper onClick={props.onClickMoveToDetail} id={el.id}>
+                  <S.Image />
+                  <S.ProfileWrapper>
+                    <S.NameWrapper>
+                      <S.NameTitle>이름</S.NameTitle>
+                      <S.Name>{el.name}</S.Name>
+                    </S.NameWrapper>
+                    <S.IntroductionWrapper>
+                      <S.IntroductionTitle>소개</S.IntroductionTitle>
+                      <S.Introduction>{el.summary}</S.Introduction>
+                    </S.IntroductionWrapper>
+                    <S.PriceWrapper>
+                      <S.PriceTitle>비용</S.PriceTitle>
+                      <S.Price>{el.price.toLocaleString()}원</S.Price>
+                    </S.PriceWrapper>
+                  </S.ProfileWrapper>
+                </S.TopWrapper>
+                <S.BottomWrapper>
+                  <AiFillStar
+                    style={{
+                      width: "27px",
+                      height: "23px",
+                      color: "#73bea8",
+                      marginRight: "5px",
+                    }}
+                  />{" "}
+                  평균 {el.averageRate} 점
+                </S.BottomWrapper>
+              </S.MapWrapper>
+            ))}
+          </InfiniteScroll>
+        </S.ListWrapper>
+      )}
     </S.Wrapper>
   );
 }

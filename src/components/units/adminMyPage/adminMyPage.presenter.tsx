ca59599adcd73e8c4cdc2f087d3615cd5e9c91f7
= <dnsghk2@gmail.com>
2022-09-15 21:14:25 +0900

    :lipstick: Add: 관리자 마이페이지 마크업

--- src/components/units/adminMyPage/adminMyPage.presenter.tsx
+++ src/components/units/adminMyPage/adminMyPage.presenter.tsx
@@ -3,20 +3,76 @@ import * as S from "./adminMyPage.styles";
 export default function AdminMyPageUI(props) {
   return (
     <S.Wrapper>
-      <InnerWrapper>
-        <Title>명언 관리</Title>
-        <button onClick={props.onClickStart}>명언 시작 버튼</button>
-        <button onClick={props.onClickStop}>명언 정지 버튼</button>
-      </InnerWrapper>
-      <InnerWrapper>
-        <Title>신고 목록 관리</Title>
-      </InnerWrapper>
-      <InnerWrapper>
-        <Title>전문가 관리</Title>
-      </InnerWrapper>
-      <InnerWrapper>
-        <Title>게시글 관리</Title>
-      </InnerWrapper>
+      <S.Title style={{ textAlign: "center" }}>관리자 페이지</S.Title>
+      <S.InnerWrapper>
+        <S.Title>명언 관리</S.Title>
+        <S.StartBtn onClick={props.onClickStart}>명언 시작 버튼</S.StartBtn>
+        <S.StopBtn onClick={props.onClickStop}>명언 정지 버튼</S.StopBtn>
+        <S.CreateBtn>명언 만들기 버튼</S.CreateBtn>
+      </S.InnerWrapper>
+      <S.InnerWrapper>
+        <S.Title>신고 목록 관리</S.Title>
+        <S.Row>
+          <S.Column__Basic>번호</S.Column__Basic>
+          <S.Column__Title>제목</S.Column__Title>
+          <S.Column__Basic>작성자</S.Column__Basic>
+          <S.Column__Basic>날짜</S.Column__Basic>
+          <S.Column__Basic>삭제</S.Column__Basic>
+          <S.Column__Basic></S.Column__Basic>
+        </S.Row>
+        <S.Row>
+          <S.Column__Basic>1</S.Column__Basic>
+          <S.Column__Title>제목</S.Column__Title>
+          <S.Column__Basic>작성자</S.Column__Basic>
+          <S.Column__Basic>날짜</S.Column__Basic>
+          <S.Column__Basic>
+            <input type="checkbox" />
+          </S.Column__Basic>
+          <S.DeleteBtn>삭제</S.DeleteBtn>
+        </S.Row>
+      </S.InnerWrapper>
+      <S.InnerWrapper>
+        <S.Title>전문가 관리</S.Title>
+        <S.Row>
+          <S.Column__Basic>번호</S.Column__Basic>
+          <S.Column__Title>제목</S.Column__Title>
+          <S.Column__Basic>작성자</S.Column__Basic>
+          <S.Column__Basic>날짜</S.Column__Basic>
+          <S.Column__Basic>삭제</S.Column__Basic>
+          <S.Column__Basic></S.Column__Basic>
+        </S.Row>
+        <S.Row>
+          <S.Column__Basic>1</S.Column__Basic>
+          <S.Column__Title>제목</S.Column__Title>
+          <S.Column__Basic>작성자</S.Column__Basic>
+          <S.Column__Basic>날짜</S.Column__Basic>
+          <S.Column__Basic>
+            <input type="checkbox" />
+          </S.Column__Basic>
+          <S.DeleteBtn>삭제</S.DeleteBtn>
+        </S.Row>
+      </S.InnerWrapper>
+      <S.InnerWrapper>
+        <S.Title>게시글 관리</S.Title>
+        <S.Row>
+          <S.Column__Basic>번호</S.Column__Basic>
+          <S.Column__Title>제목</S.Column__Title>
+          <S.Column__Basic>작성자</S.Column__Basic>
+          <S.Column__Basic>날짜</S.Column__Basic>
+          <S.Column__Basic>삭제</S.Column__Basic>
+          <S.Column__Basic></S.Column__Basic>
+        </S.Row>
+        <S.Row>
+          <S.Column__Basic>1</S.Column__Basic>
+          <S.Column__Title>제목</S.Column__Title>
+          <S.Column__Basic>작성자</S.Column__Basic>
+          <S.Column__Basic>날짜</S.Column__Basic>
+          <S.Column__Basic>
+            <input type="checkbox" />
+          </S.Column__Basic>
+          <S.DeleteBtn>삭제</S.DeleteBtn>
+        </S.Row>
+      </S.InnerWrapper>
     </S.Wrapper>
   );
 }
--- src/components/units/adminMyPage/adminMyPage.styles.ts
+++ src/components/units/adminMyPage/adminMyPage.styles.ts
@@ -1,5 +1,80 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
+  min-width: 1200px;
   width: 100vw;
+  padding: 50px;
+`;
+
+export const InnerWrapper = styled.div`
+  width: 100%;
+  height: 30%;
+  margin-bottom: 30px;
+`;
+
+export const Title = styled.h1`
+  font-weight: 800;
+  font-size: 1.7rem;
+  margin-bottom: 30px;
+`;
+
+export const StartBtn = styled.button`
+  width: 150px;
+  height: 100px;
+  border: none;
+  font-size: 1.2rem;
+  margin-right: 30px;
+  cursor: pointer;
+`;
+
+export const StopBtn = styled.button`
+  width: 150px;
+  height: 100px;
+  border: none;
+  font-size: 1.2rem;
+  margin-right: 30px;
+  cursor: pointer;
+`;
+
+export const CreateBtn = styled.button`
+  width: 150px;
+  height: 100px;
+  border: none;
+  font-size: 1.2rem;
+  margin-right: 30px;
+  cursor: pointer;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 52px;
+  line-height: 52px;
+  border-bottom: 1px solid gray;
+`;
+
+export const Column__Basic = styled.div`
+  width: 10%;
+  text-align: center;
+  cursor: pointer;
+`;
+
+export const Column__Title = styled.div`
+  width: 50%;
+  text-align: center;
+  cursor: pointer;
+`;
+
+export const DeleteBtn = styled.button`
+  width: 113px;
+  height: 30px;
+  background-color: #73c7a0;
+  border: none;
+  cursor: pointer;
+  border-radius: 30px;
+  font-size: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
 `;

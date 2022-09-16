f7b06d1e07ff459983dbc854928e1ea778abdb4d
Hailey0930 <hyerin0930@gmail.com>
2022-09-16 17:10:23 +0900

    :art: Edit : 유저 마이페이지 활동기록 API 구현 #2

--- src/components/units/my-page/my-page.types.ts
+++ src/components/units/my-page/my-page.types.ts
@@ -22,4 +22,28 @@ export interface IMyPagePresenterProps {
   onChangeReview: (event: ChangeEvent<HTMLTextAreaElement>) => void;
   reviewIsActive: boolean;
   onClickReviewSubmit: () => void;
+  community: boolean;
+  comment: boolean;
+  like: boolean;
+  communityData: Pick<IQuery, "fetchOwnStories"> | undefined;
+  FetchMoreCommunityData: () => void;
+  commentData: Pick<IQuery, "fetchOwnComments"> | undefined;
+  FetchMoreCommentData: () => void;
+  likeData: Pick<IQuery, "fetchOwnLikedStories"> | undefined;
+  FetchMoreLikeData: () => void;
+  onClickCommunity: () => void;
+  onClickComment: () => void;
+  onClickLike: () => void;
+}
+
+export interface ICommunityProps {
+  community: boolean;
+}
+
+export interface ICommentProps {
+  comment: boolean;
+}
+
+export interface ILikeProps {
+  like: boolean;
 }
